43be9177881976571b22fac62f6a039f35b679e6
Mohammad A.Y <Mohammad.dd.MY@gmail.com>
2026-02-14 20:35:41 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://lh3.goo

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -37,6 +38,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'gen-lang-client-0789065518.firebasestorage.app',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
       },
